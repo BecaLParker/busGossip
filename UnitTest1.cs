5cd16eda34b5a8d54f87642f177298425abfce00
BecaLParker <beca.parker@red-gate.com>
2023-09-22 10:53:06 +0100

    WIP WhenTwoDrivers meet

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -15,12 +15,39 @@ public class Tests
         // assert
         Assert.That(result, Is.EqualTo(1));
     }
+    
+    [Test]
+    public void WhenTwoDriversMeet_ShouldReturnNumberOfStops()
+    {
+        // arrange
+        
+
+        // act
+        var result = Program.GetNumberOfStops(new List<int> { 3, 1 }, new List<int> { 2, 1 });
+
+        // assert
+        Assert.That(result, Is.EqualTo(2));
+    }
 }
 
 internal class Program
 {
-    internal static int GetNumberOfStops(List<int> list)
+    internal static int GetNumberOfStops(params List <int>[] routes)
     {
-        return 1;
+        if (routes.Length == 1)
+        {
+            return 1;
+        }
+        var route1 = routes[0];
+        var route2 = routes[1];
+       for (int i = 0; i < route1.Count; i++)
+       {
+           if (route1[i] == route2[i])
+           {
+               return i + 1;
+           }
+       }
+
+       throw new Exception("Jeff and Beca are confused);
     }
 }
